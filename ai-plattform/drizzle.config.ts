b08cf5998437d9db618e7ecf6d247495b84e4bff
AlainDLC <jd_alain@hotmail.com>
2025-02-15 15:19:48 +0100

    backend fix

--- ai-plattform/drizzle.config.ts
+++ ai-plattform/drizzle.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   schema: "./configs/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.NEXT_PUBLIC_DATABASE_URL!,
+    url: "",
   },
 });
